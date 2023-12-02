f2cf447d8a7f40507c5afebea13b876286e76f8e
Marguerite Aya Cinderella Gautier <115854212+margueriteaya@users.noreply.github.com>
2023-12-02 21:15:25 +0900

    Initial CHS translation

--- questions.js
+++ questions.js
@@ -1,6 +1,6 @@
 questions = [
   {
-      "question": "Only people who wish to or have undergone medical transition, should speak for the trans community",
+      "question": "只有想要或正在进行跨性别医疗（激素替代、性别重置手术等）的人，才有资格为跨性别群体讲话。",
       "effect": {
           "socials": 2,
           "trus": -3,
@@ -9,7 +9,7 @@ questions = [
       }
   },
   {
-      "question": "Autogynephelia is a real phenomenon",
+      "question": "自我女性恋真实存在。译者注：自我女性恋(Autogynephilia)是一种性欲倒错模型，声称跨性别女性如果不只受男性吸引，则其本质是喜欢女性的男性，性别转换的动机是自身作为女性的想法或形象、将自己设想为女性而获取的性兴奋。又名“幻想变性性兴奋理论”。",
       "effect": {
           "socials": 5,
           "trus": 0,
@@ -18,7 +18,7 @@ questions = [
       }
   },
   {
-      "question": "Being trans can be a choice",
+      "question": "成为跨性别可以是一个选择。",
       "effect": {
           "socials": -5,
           "trus": 5,
@@ -27,7 +27,7 @@ questions = [
       }
   },
   {
-      "question": "\"pronouns in bio\" do nothing to help trans people and exist only to make cis people feel good about themselves",
+      "question": "顺性别者在个人资料中使用代词对跨性别群体没有帮助，只是让他们自己感觉做了好事。",
       "effect": {
           "socials": 5,
           "trus": -2,
@@ -36,7 +36,7 @@ questions = [
       }
   },
   {
-      "question": "Cis women are justified to be uncomfortable in bathrooms around masculine presenting trans women",
+      "question": "顺女在厕所里看见形象偏男的跨性别女性感到不适是正当的。",
       "effect": {
           "socials": 5,
           "trus": -2,
@@ -45,7 +45,7 @@ questions = [
       }
   },
   {
-      "question": "All trans women can be grouped into 2 categories: AGP and HSTS",
+      "question": "所有的跨女都能分成两种，一种是意淫自己是女同的屌丝，另一种是意淫自己是女性的基佬。",
       "effect": {
           "socials": 7,
           "trus": -2,
@@ -54,7 +54,7 @@ questions = [
       }
   },
   {
-      "question": "The goal of transitioning is \"passing\" as the opposite gender",
+      "question": "跨性别的尽头就是被路人认成自己所认同的性别。",
       "effect": {
           "socials": 4,
           "trus": -1,
@@ -63,7 +63,7 @@ questions = [
       }
   },
   {
-      "question": "Even if it is a dead meme, \"attack helicopter\" would theoretically be a valid gender identity",
+      "question": "虽然是一个烂梗，但是“武装直升机”确实可以作为一个性别认同。",
       "effect": {
           "socials": -6,
           "trus": 4,
@@ -72,7 +72,7 @@ questions = [
       }
   },
   {
-      "question": "Neopronouns are valid",
+      "question": "多元性别群体所创造的新代词的存在是合理的。译者注：新代词在英文中包含\"ze\", \"zim\", \"zir\"等；在中文中包含“男也”、“〆也”、“狝”等。",
       "effect": {
           "socials": -6,
           "trus": 4,
@@ -81,7 +81,7 @@ questions = [
       }
   },
   {
-      "question": "If you don't experience gender dysphoria, you are not transgender",
+      "question": "如果你没有性别不安，你就不是个跨性别。",
       "effect": {
           "socials": 2,
           "trus": -7,
@@ -90,7 +90,7 @@ questions = [
       }
   },
   {
-      "question": "Xenogenders (stargender, etc...) are stupid",
+      "question": "超人类性别（星星性别、鼠鼠性别等等，不根据二元性别来定义而根据其他与性别无关的事物来定义）看起来有点低能。",
       "effect": {
           "socials": 6,
           "trus": -3,
@@ -99,7 +99,7 @@ questions = [
       }
   },
   {
-      "question": "Trans people are often unnecessarily harsh on themselves and their own appearance",
+      "question": "跨儿们没有必要苛求自己和自己的外表打扮。",
       "effect": {
           "socials": -2,
           "trus": 0,
@@ -108,7 +108,7 @@ questions = [
       }
   },
   {
-      "question": "Trans people must disclose they are trans before having sex with someone, regardless of past surgeries",
+      "question": "即使是术后，跨性别在和人做爱前也应该告诉对方自己是跨性别者。",
       "effect": {
           "socials": 3,
           "trus": 0,
@@ -117,7 +117,7 @@ questions = [
       }
   },
   {
-      "question": "Trans people can never feel free in cisnormative society",
+      "question": "在一个以顺直人为主导、视顺直人为正常的社会，跨儿们永远都没有自己的自由。",
       "effect": {
           "socials": -3,
           "trus": 3,
@@ -126,7 +126,7 @@ questions = [
       }
   },
   {
-      "question": "DIY HRT is generally safe and effective",
+      "question": "自行用药（指自行购入激素药物并规划用药）大体上是安全有效的。",
       "effect": {
           "socials": 0,
           "trus": 0,
@@ -135,7 +135,7 @@ questions = [
       }
   },
   {
-      "question": "DIY HRT for transmascs should be legal, even if it enables steroid usage",
+      "question": "跨性别男性自购激素药物应当合法，即使这可能造成类固醇滥用。",
       "effect": {
           "socials": 0,
           "trus": 0,
@@ -144,7 +144,7 @@ questions = [
       }
   },
   {
-      "question": "The goal of transitioning is to blend into cis society",
+      "question": "性别转换的目标是融入顺直人圈子里。",
       "effect": {
           "socials": 0,
           "trus": 0,
@@ -153,7 +153,7 @@ questions = [
       }
   },
   {
-      "question": "Trans people need to unapologetically be themselves, regardless of societies opinion",
+      "question": "不管社会看法如何，跨儿们都应该毫无歉意地做自己，不应有任何顾忌。",
       "effect": {
           "socials": -5,
           "trus": 2,
@@ -162,7 +162,7 @@ questions = [
       }
   },
   {
-      "question": "I know what the word \"tripfag\" means",
+      "question": "我知道“打赢复活赛”意味着什么。",
       "effect": {
           "socials": 8,
           "trus": 0,
@@ -171,7 +171,7 @@ questions = [
       }
   },
   {
-      "question": "The medical risks of HRT are overblown",
+      "question": "激素药物的风险性被夸大了。",
       "effect": {
           "socials": 0,
           "trus": 0,
@@ -180,7 +180,7 @@ questions = [
       }
   },
   {
-      "question": "The 4chan trans subculture is toxic and actively makes trans people within it miserable",
+      "question": "清蒸协会所代表的社群文化是有毒的，这甚至让其中的人们也痛苦不堪。",
       "effect": {
           "socials": -4,
           "trus": 0,
@@ -189,7 +189,7 @@ questions = [
       }
   },
   {
-      "question": "Nonbinary dysphoria exists",
+      "question": "作为一个非二元，我的性别焦虑真实存在。",
       "effect": {
           "socials": 0,
           "trus": -3,
@@ -198,7 +198,7 @@ questions = [
       }
   },
   {
-      "question": "The absence of urinals in womens toilets is transphobic",
+      "question": "女厕里没有站立小便池是一种恐跨。",
       "effect": {
           "socials": -4,
           "trus": 0,
@@ -207,7 +207,7 @@ questions = [
       }
   },
   {
-      "question": "Gender dysphoria is a mental illness",
+      "question": "性别不安是一种精神疾病。",
       "effect": {
           "socials": 6,
           "trus": -4,
@@ -216,7 +216,7 @@ questions = [
       }
   },
   {
-      "question": "DIY HRT is better than a prescription because it gives you more options and freedom",
+      "question": "自行用药比遵循医嘱更好，它提供的选择面更加宽泛。",
       "effect": {
           "socials": 1,
           "trus": 0,
@@ -225,7 +225,7 @@ questions = [
       }
   },
   {
-      "question": "Most endocrinologists providing trans healthcare are incompetent",
+      "question": "大多数内分泌医生在跨性别用药问题上都不称职。",
       "effect": {
           "socials": 0,
           "trus": 0,
@@ -234,7 +234,7 @@ questions = [
       }
   },
   {
-      "question": "Helping minors access the resources to purchase DIY HRT is morally reprehensible",
+      "question": "帮助低龄跨儿自行获得激素药物应在道德上受谴责。",
       "effect": {
           "socials": 0,
           "trus": 0,
@@ -243,7 +243,7 @@ questions = [
       }
   },
   {
-      "question": "When it comes to trans healthcare, people should trust their doctors over what they read online",
+      "question": "关于跨性别医疗，比起网路社群提供的资料，医生更值得信赖。",
       "effect": {
           "socials": 0,
           "trus": 0,
@@ -252,7 +252,7 @@ questions = [
       }
   },
   {
-      "question": "Trans liberation through assimilation",
+      "question": "跨性别群体的解放应通过融入社会、与大众同化来争取。",
       "effect": {
           "socials": 0,
           "trus": 0,
@@ -261,7 +261,7 @@ questions = [
       }
   },
   {
-      "question": "A lot of people are trans purely for attention and brownie points",
+      "question": "很多人当跨性别都是为了博取眼球，显得自己与众不同。",
       "effect": {
           "socials": 5,
           "trus": -5,
@@ -270,7 +270,7 @@ questions = [
       }
   },
   {
-      "question": "HRT should only be a last resort, not a first line treatment",
+      "question": "不到最后，跨性别就不该使用激素药物改变自己的身体。",
       "effect": {
           "socials": 0,
           "trus": 0,
@@ -279,7 +279,7 @@ questions = [
       }
   },
   {
-      "question": "Gender abolitionism is desirable",
+      "question": "“废除一切性别概念”的观点有其可取之处。",
       "effect": {
           "socials": -5,
           "trus": 2,
@@ -288,7 +288,7 @@ questions = [
       }
   },
   {
-      "question": "America's \"informed consent\" model for HRT is preferable to European trans healthcare",
+      "question": "美国的激素药物知情同意模式应该推广到中国的跨性别医疗领域，以取代当下实行的医生给出易性症诊断证明才可获取药物的规定。",
       "effect": {
           "socials": 0,
           "trus": 2,
@@ -297,7 +297,7 @@ questions = [
       }
   },
   {
-      "question": "Genital preferences are transphobic",
+      "question": "“我只想和有阴茎的男性/有阴道的女性做爱”是一种恐跨行为。",
       "effect": {
           "socials": 0,
           "trus": 1,
@@ -306,7 +306,7 @@ questions = [
       }
   },
   {
-      "question": "HRT should never be given without psychiatric evaluation",
+      "question": "没有医生的“易性症”诊断不应该获取激素治疗。",
       "effect": {
           "socials": 0,
           "trus": -2,
@@ -315,7 +315,7 @@ questions = [
       }
   },
   {
-      "question": "Online mainstream trans discourse is dominated by cis men who dont understand trans issues",
+      "question": "网路上讨论跨性别时，大多数时候的主流舆论核心是一群啥几把都不懂的顺直男。",
       "effect": {
           "socials": -3,
           "trus": 0,
@@ -324,7 +324,7 @@ questions = [
       }
   },
   {
-      "question": "Eugenics through gene editing to end gender dysphoria is desirable",
+      "question": "通过优生学的方法修改基因，以达到根除性别不安，这是一项可取的举措。",
       "effect": {
           "socials": 3,
           "trus": -2,
